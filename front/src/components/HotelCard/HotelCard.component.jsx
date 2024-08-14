94082c0e1260c3e1fb3686aa6d9c6526f3dbc2d4
David Blervaque <davidblervaque@gmail.com>
2024-08-14 14:17:05 +0200

    feat: style on score and review

--- front/src/components/HotelCard/HotelCard.component.jsx
+++ front/src/components/HotelCard/HotelCard.component.jsx
@@ -24,11 +24,14 @@ const HotelCardBase = ({
 }) => (
   <div className="hotel__card">
     <img src={pictureId} />
-    <div className="hotel__card__name__container">
-      <p className='hotel__card__name'>{name}</p>
-      <StarBase stars={stars} />
-      <div>
-        {avgScore+''} ({reviewCount+''})
+    <div className='hotel__card__top__line__container'>
+      <div className="hotel__card__name__container">
+        <p className='hotel__card__name'>{name}</p>
+        <StarBase stars={stars} />
+      </div>       
+      <div className='hotel__card__score__container'>
+        <p className='hotel__card__avg__score'>{avgScore+''}</p>
+        <p className='hotel__card__total_reviews'>({reviewCount+''})</p>
       </div>
     </div>
     <p>{preview}</p>
